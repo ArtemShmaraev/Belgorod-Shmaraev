27a5ec0866f03900b7d1855f76f658656db2278d
ArtemShmaraev <74972298+ArtemShmaraev@users.noreply.github.com>
2021-02-19 14:47:00 +0300

    Update Поиск Аптеки.py

--- Поиск Аптеки.py
+++ Поиск Аптеки.py
@@ -1,49 +1,70 @@
-from io import BytesIO
+import argparse
 import requests
 from PIL import Image
-import argparse
+from io import BytesIO
+import math
+
+def lonlat_distance(a_lon, a_lat, b_lon, b_lat):
+    degree_to_meters_factor = 111 * 1000 # 111 километров в метрах
+    radians_lattitude = math.radians((a_lat + b_lat) / 2.)
+    lat_lon_factor = math.cos(radians_lattitude)
+    dx = abs(a_lon - b_lon) * degree_to_meters_factor * lat_lon_factor
+    dy = abs(a_lat - b_lat) * degree_to_meters_factor
+    distance = math.sqrt(dx * dx + dy * dy)
+
+    return distance
+
+
 
 parser = argparse.ArgumentParser()
 parser.add_argument("adress", nargs="+")
-toponym_to_find = " ".join(parser.parse_args().adress)
+adr = " ".join(parser.parse_args().adress)
 
-geocoder_api_server = "http://geocode-maps.yandex.ru/1.x/"
-
-geocoder_params = {
-    "apikey": "40d1649f-0493-4b70-98ba-98533de7710b",
-    "geocode": toponym_to_find,
-    "format": "json"}
-
-response = requests.get(geocoder_api_server, params=geocoder_params)
-
-if not response:
-
-    pass
-
+geocoder_request = "http://geocode-maps.yandex.ru/1.x/?apikey=40d1649f-0493-4b70-98ba-98533de7710b&" \
+                   f"geocode={adr}&format=json"
+response = requests.get(geocoder_request)
 json_response = response.json()
-toponym = json_response["response"]["GeoObjectCollection"][
-    "featureMember"][0]["GeoObject"]
-maxm = list(map(float, toponym["boundedBy"]["Envelope"]["upperCorner"].split(" ")))
-minim = list(map(float, toponym["boundedBy"]["Envelope"]["lowerCorner"].split(" ")))
-d = str(maxm[0] - minim[0])
-sh = str(maxm[1] - minim[1])
-s = [d, sh]
+toponym = json_response["response"]["GeoObjectCollection"]["featureMember"][0]["GeoObject"]
+toponym_address = toponym["metaDataProperty"]["GeocoderMetaData"]["text"]
 toponym_coodrinates = toponym["Point"]["pos"]
 toponym_longitude, toponym_lattitude = toponym_coodrinates.split(" ")
 
-delta = "0.005"
+
+search_api_server = "https://search-maps.yandex.ru/v1/"
+api_key = "dda3ddba-c9ea-4ead-9010-f43fbc15c6e3"
 
 
-map_params = {
+search_params = {
+    "apikey": api_key,
+    "text": "аптека",
+    "lang": "ru_RU",
     "ll": ",".join([toponym_longitude, toponym_lattitude]),
-    "l": "map",
-    "pt": ",".join([toponym_longitude, toponym_lattitude]) + ",pm2pnl",
-    "spn": ",".join(s)
+    "type": "biz"
 }
 
-map_api_server = "http://static-maps.yandex.ru/1.x/"
-response = requests.get(map_api_server, params=map_params)
-print(response.url)
+response = requests.get(search_api_server, params=search_params)
+json_response = response.json()
+organization = json_response["features"][0]
+org_name = organization["properties"]["CompanyMetaData"]["name"]
+org_address = organization["properties"]["CompanyMetaData"]["address"]
+point = organization["geometry"]["coordinates"]
+org_point = "{0},{1}".format(point[0], point[1])
 
-Image.open(BytesIO(
-    response.content)).show()
+
+s = round(lonlat_distance(float(toponym_longitude), float(toponym_lattitude), float(point[0]), float(point[1])))
+print(f"Aдрес аптеки: {org_address}")
+print(f"Название аптеки: {org_name}")
+print(f"Расстояние до аптеки {s} м")
+
+
+map_api_server = "http://static-maps.yandex.ru/1.x/"
+map_params = {
+    # позиционируем карту центром на наш исходный адрес
+    "ll": ",".join([toponym_longitude, toponym_lattitude]),
+    "l": "map",
+    # добавим точку, чтобы указать найденную аптеку
+    "pt": "{0},pm2dgl~{1},ya_ru".format(org_point, ",".join([toponym_longitude, toponym_lattitude]),)
+}
+response = requests.get(map_api_server, params=map_params)
+Image.open(BytesIO(response.content)).show()
+
